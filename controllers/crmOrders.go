4def82560a8227d8539c09d35bc186eb77cf7372
JoanGTSQ <49458356+JoanGTSQ@users.noreply.github.com>
2024-03-14 15:16:33 +0000

    newnew

--- controllers/crmOrders.go
+++ controllers/crmOrders.go
@@ -3,10 +3,10 @@ package controllers
 import (
 	"jgt.solutions/errorController"
 
+	"fmt"
 	"jgt.solutions/models"
-	"net/http"    
-    "fmt"
-    "strconv"
+	"net/http"
+	"strconv"
 
 	"jgt.solutions/views"
 )
@@ -61,55 +61,69 @@ func (c *Crm) CreateOrder(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var products []*models.OrderProductMaterial
-    	for i := 0; ; i++ {
-    		productID := r.FormValue(fmt.Sprintf("products[%d][productID]", i))
-    		if productID == "" {
-    			break // Salir del bucle si no hay más productos
-    		}
-    		materialID := r.FormValue(fmt.Sprintf("products[%d][materialID]", i))
-            errorController.InfoLogger.Println(productID, materialID)
-    		// Convertir los ID de productos y materiales a int64
-    		productIDInt, err := strconv.ParseInt(productID, 10, 64)
-    		if err != nil {
-    			// Manejar el error
-    			http.Error(w, fmt.Sprintf("Error al obtener el ID del producto %d", i), http.StatusInternalServerError)
-    			return
-    		}
-    		materialIDInt, err := strconv.ParseInt(materialID, 10, 64)
-    		if err != nil {
-    			// Manejar el error
-    			http.Error(w, fmt.Sprintf("Error al obtener el ID del material del producto %d", i), http.StatusInternalServerError)
-    			return
-    		}
-            product, err := c.crm.SearchProductByID(productIDInt)
-            if err != nil {
-                errorController.ErrorLogger.Println("Error al buscar el producto:", err)
-                return // Continuar con el siguiente producto si hay un error
-            }
+	for i := 0; ; i++ {
+		productID := r.FormValue(fmt.Sprintf("products[%d][productID]", i))
+		if productID == "" {
+			break // Salir del bucle si no hay más productos
+		}
+		materialID := r.FormValue(fmt.Sprintf("products[%d][materialID]", i))
+		cost := r.FormValue(fmt.Sprintf("products[%d][cost]", i))
+		sale := r.FormValue(fmt.Sprintf("products[%d][sale]", i))
+		quality := r.FormValue(fmt.Sprintf("products[%d][quality]", i))
+		// Convertir los ID de productos y materiales a int64
+		productIDInt, err := strconv.ParseInt(productID, 10, 64)
+		if err != nil {
+			// Manejar el error
+			http.Error(w, fmt.Sprintf("Error al obtener el ID del producto %d", i), http.StatusInternalServerError)
+			return
+		}
+		materialIDInt, err := strconv.ParseInt(materialID, 10, 64)
+		if err != nil {
+			// Manejar el error
+			http.Error(w, fmt.Sprintf("Error al obtener el ID del material del producto %d", i), http.StatusInternalServerError)
+			return
+		}
+		productCost, err := strconv.ParseFloat(cost, 64)
+		if err != nil {
+			// Manejar el error
+			http.Error(w, fmt.Sprintf("Error al obtener el ID del material del producto %d", i), http.StatusInternalServerError)
+			return
+		}
+		productSale, err := strconv.ParseFloat(sale, 64)
+		if err != nil {
+			// Manejar el error
+			http.Error(w, fmt.Sprintf("Error al obtener el ID del material del producto %d", i), http.StatusInternalServerError)
+			return
+		}
+		product, err := c.crm.SearchProductByID(productIDInt)
+		if err != nil {
+			errorController.ErrorLogger.Println("Error al buscar el producto:", err)
+			return // Continuar con el siguiente producto si hay un error
+		}
 
-            material, err := c.crm.SearchMaterialByID(materialIDInt)
-            if err != nil {
-                errorController.ErrorLogger.Println("Error al buscar el material:", err)
-                return // Continuar con el siguiente producto si hay un error
-            }
-            material.Weight -= product.Weight
-            products = append(products, &models.OrderProductMaterial{
-                
-                Product:  *product,
-                Material: *material,
-                Quality:  product.Quality,
-            })
-            totalTime += product.TimeMinutes
-            // TODO: Calcular el costo y la venta adecuadamente
-            totalCost += 1
-            totalSale += product.Price
-            // Actualizar el peso del material
-            err = c.crm.UpdateMaterial(material)
-            if err != nil {
-                errorController.ErrorLogger.Println("Error al actualizar el material:", err)
-                continue // Continuar con el siguiente producto si hay un error
-            }
-    	}
+		material, err := c.crm.SearchMaterialByID(materialIDInt)
+		if err != nil {
+			errorController.ErrorLogger.Println("Error al buscar el material:", err)
+			return // Continuar con el siguiente producto si hay un error
+		}
+		material.Weight -= product.Weight
+		products = append(products, &models.OrderProductMaterial{
+
+			Product:  *product,
+			Material: *material,
+			Quality:  quality,
+		})
+		totalTime += product.TimeMinutes
+		// TODO: Calcular el costo y la venta adecuadamente
+		totalCost += productCost
+		totalSale += productSale
+		// Actualizar el peso del material
+		err = c.crm.UpdateMaterial(material)
+		if err != nil {
+			errorController.ErrorLogger.Println("Error al actualizar el material:", err)
+			continue // Continuar con el siguiente producto si hay un error
+		}
+	}
 
 	// Crear la orden en la base de datos
 	order := models.Order{
